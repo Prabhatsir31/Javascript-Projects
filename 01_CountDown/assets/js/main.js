d010aec5c2108ce48e19b205aa7b7717a0c5f39b
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-15 12:22:44 +0530

    Update main.js

--- 01_CountDown/assets/js/main.js
+++ 01_CountDown/assets/js/main.js
@@ -1,35 +1,32 @@
 const endDate = "31 December 2024 12:00 AM"
 
-document.getElementById("end-date").innerHTML = endDate;
+document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
-
-
+   
 function clock() {
-    const end = new Date(endDate);
+    const end = new Date(endDate)
     const now = new Date()
     const diff = (end - now) / 1000;
-    if(diff < 0) return;
-    
-    // convert into Days
+
+    if (diff < 0) return;
+
+    // convert into days;
     inputs[0].value = Math.floor(diff / 3600 / 24);
-    // convert into Hrs
     inputs[1].value = Math.floor(diff / 3600) % 24;
-    // convert into Min
     inputs[2].value = Math.floor(diff / 60) % 60;
-    // convert into Sec
     inputs[3].value = Math.floor(diff) % 60;
 }
 
 // initial call
 clock()
 
-/* 
-    1 day = 24 hrs
-    1 hrs = 60 min
-    1 min = 3600 sec
-*/
+/**
+ *  1 day = 24 hrs
+ *  1 hr = 60 mins
+ *  60 min = 3600 sec
+ */
 
-setInterval() (
+setInterval(
     () => {
         clock()
     },
