93dbf6a7f248048b27496597cc8ad01a4e42230f
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-27 22:16:18 +0530

    Update main.js

--- 07_Randon Para/assets/js/main.js
+++ 07_Randon Para/assets/js/main.js
@@ -11,7 +11,6 @@ const lowerInput = document.getElementById("lower-case")
 const numberInput = document.getElementById("numbers")
 const symbolInput = document.getElementById("symbols")
 
-
 const getRandomData = (dataSet) => {
     return dataSet[Math.floor(Math.random() * dataSet.length)]
 }
