892e0ad1dc1063d85dd38c7130514099a1bb1d9f
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-29 21:02:32 +0530

    Add files via upload

---
+++ 11_QR Generator/assets/js/main.js
@@ -0,0 +1,20 @@
+
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generatorQR()
+{
+    if (qrText.value.length > 0) 
+    {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    }else{
+        qrText.classList.add('error');
+        setTimeout(()=> {
+            qrText.classList.remove('remove');
+        },1000);
+    }
+
+   
+}
