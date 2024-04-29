57bcb46ab75ffcb4bcc917a8044618e6296a2c83
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-29 20:34:00 +0530

    Update main.js

--- 10_Quote Generator/assets/js/main.js
+++ 10_Quote Generator/assets/js/main.js
@@ -1,15 +1,7 @@
 ith(y1, m1) + d2 - d1;
     }  
 
-    if(m3 < 0)
-    {
-        m3 = 11;
-        y3--;
-    }
-    console.log(y3,m3,d3);
-    result.innerHTML = `You are <span> ${y3} </span> years, <span> ${m3} </span> months, and <span> ${d3} </span> days old.`
-    
-}
+  
 
 function getDaysInMonth(year, month)
 {
