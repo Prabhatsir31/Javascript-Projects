5b3c294740626bfc2731773791662ff3debbf9f9
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-29 21:22:00 +0530

    Add files via upload

---
+++ 12_Toast Notification/assets/js/main.js
@@ -0,0 +1,22 @@
+let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fas fa-check-circle"></i> Successfully Submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
+let invalidMsg = '<i class="fas fa-exclamation"></i> Invalid input, check again';
+
+function showToast(msg) {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('error')) {
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')) {
+        toast.classList.add('invalid'); // Update to 'invalid'
+    }
+
+    setTimeout(() => {
+        toast.remove();
+    }, 6000);
+}
