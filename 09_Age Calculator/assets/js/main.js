b4d8a04fd3d4ea3bd693f10800928001c649bd41
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-29 12:32:10 +0530

    Update main.js

--- 09_Age Calculator/assets/js/main.js
+++ 09_Age Calculator/assets/js/main.js
@@ -1,3 +1,5 @@
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
 let result = document.getElementById("result");
+
+function calculateAge(){
