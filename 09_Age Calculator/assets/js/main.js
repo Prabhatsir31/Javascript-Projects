0ba3a1d74109b133bfec602199ae26db5a1749ff
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-29 12:37:44 +0530

    Update main.js

--- 09_Age Calculator/assets/js/main.js
+++ 09_Age Calculator/assets/js/main.js
@@ -38,4 +38,5 @@ function calculateAge(){
         m3 = 11;
         y3--;
     }
+  result.innerHTML = `You are <span> ${y3} </span> years, <span> ${m3} </span> months, and <span> ${d3} </span> days old.`
 }
