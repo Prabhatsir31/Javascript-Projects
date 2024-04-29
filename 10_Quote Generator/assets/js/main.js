2564d7552de66c87f60f074e9a30a9917ec47242
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-29 20:31:52 +0530

    Update main.js

--- 10_Quote Generator/assets/js/main.js
+++ 10_Quote Generator/assets/js/main.js
@@ -1,6 +1,4 @@
-let userInput = document.getElementById("date");
-userInput.max = new Date().toISOString().split("T")[0];
-let result = document.getElementById("result");
+
 
 function calculateAge(){
     let birthDate = new Date(userInput.value);
