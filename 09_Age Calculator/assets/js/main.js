712d70b7ca89d9651a5108fb905f02fa20a2f263
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-29 12:37:00 +0530

    Update main.js

--- 09_Age Calculator/assets/js/main.js
+++ 09_Age Calculator/assets/js/main.js
@@ -26,4 +26,12 @@ function calculateAge(){
         y3--;
         m3 = 12 + m2 - m1;
    }
+  if(d2 >= d1)
+    {
+        d3 = d2 - d1;
+    }else{
+       m3--;
+       d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }  
+
 }
