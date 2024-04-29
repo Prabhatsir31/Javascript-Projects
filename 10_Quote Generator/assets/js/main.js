7c8e7dd20e52ead25269e1b43b2d44f003f40315
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-29 20:49:14 +0530

    Update main.js

--- 10_Quote Generator/assets/js/main.js
+++ 10_Quote Generator/assets/js/main.js
@@ -1,3 +1,5 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 // const tags = document.getElementById("tags");
+
+const api_url = "https://api.quotable.io/random";
