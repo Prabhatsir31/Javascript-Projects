a0e830060be00ff1197cee21a1bec04741fdc943
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-01 09:30:33 +0530

    Update music-list.js

--- 13_Music Player/assets/js/music-list.js
+++ 13_Music Player/assets/js/music-list.js
@@ -83,4 +83,46 @@ let allMusic = [
     img: "music-7",
     src: ""
   },
+    {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: ""
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: ""
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: ""
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: ""
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: ""
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: ""
+  },
+  {
+    name: "Pehle Bhi Mein",
+    artist: "Arjit Singh",
+    img: "music-7",
+    src: ""
+  },
 ];
