447b583c472a2796269b3d21f8ce19025a3d169d
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-05 21:20:11 +0530

    Add files via upload

---
+++ 23_Drag & Drop/assets/js/script.js
@@ -0,0 +1,30 @@
+let lists = document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for (let list of lists) {
+    list.addEventListener('dragstart', function (e) {
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover", function (e) {
+            e.preventDefault();
+        });
+
+        rightBox.addEventListener("drop", function (e) {
+            e.preventDefault();
+            rightBox.appendChild(selected);
+            selected = null;
+        });
+
+        leftBox.addEventListener("dragover", function (e) {
+            e.preventDefault();
+        });
+
+        leftBox.addEventListener("drop", function (e) {
+            e.preventDefault();
+            leftBox.appendChild(selected);
+            selected = null;
+        });
+
+    });
+}
