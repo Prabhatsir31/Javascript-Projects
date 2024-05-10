c831ff5324da93fbde3539282bd5d45ed34b398e
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-10 10:21:00 +0530

    Update script.js

--- 25_Javascript API Projects/25.2_Weather App/assets/js/script.js
+++ 25_Javascript API Projects/25.2_Weather App/assets/js/script.js
@@ -1,82 +1,63 @@
-const searchBox = document.querySelector(".searchBox");
-const searchBtn = document.querySelector(".searchBtn");
-const recipeContainer = document.querySelector(".recipe-container");
-const recipeCloseBtn = document.querySelector(".recipe-close-btn");
-const recipeDetailsContent = document.querySelector(".recipe-details-content");
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
 
-const fetchRecipes = async (query) => {
-    try
-    {
-        const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
-        const response = await data.json();
-    
-        recipeContainer.innerHTML = "";
-        response.meals.forEach(meal => {
-            const recipeDiv = document.createElement('div');
-            recipeDiv.classList.add('recipe');
-            recipeDiv.innerHTML = `
-            <img src="${meal.strMealThumb}">
-            <h3>${meal.strMeal}</h3>
-            <p><span>${meal.strArea}</span> Dish</p>
-            <p>Belong to <span>${meal.strCategory}</span> Category</p>
-            `
-            const button = document.createElement('button');
-            button.textContent= "View Recipe";
-            recipeDiv.appendChild(button);
-    
-            // Ading EventListener to recipe button
-            button.addEventListener('click', ()=> {
-                openRecipePopup(meal);
-            });
-    
-            recipeContainer.appendChild(recipeDiv);
-        });
-    }
-    catch (error) {
-        recipeContainer.innerHTML = "<h2>Error in Fetching Recipes...</h2>";
-    }
-    
-}
+const location_not_found = document.querySelector('.location-not-found');
 
-// Function to fetch ingreients and meaasurements
-const fetchIngredients = (meal) => {
-    let ingredientsList = "";
-    for (let i = 1; i <=20; i++) {
-        const ingredient = meal[`strIngredient${i}`];
-        if(ingredient)
-        {
-            const measure = meal[`strMeasure${i}`];
-            ingredientsList += `<i>${measure} ${ingredient}</i>`
-        }
-        else{
-             break;
-        }
-    }
-    return ingredientsList;
-}
-const openRecipePopup = (meal) => {
-    recipeDetailsContent.innerHTML = `
-        <h2 class="recipeName">${meal.strMeal}</h2>
-        <h3>Ingredients:</h3>
-        <ul class="ingredientsList">${fetchIngredients(meal)}</ul>
-        <div class="recipeIntructions">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
-        </div>
-    `
-    recipeDetailsContent.parentElement.style.display = "block";
-}
+const weather_body = document.querySelector('.weather-body');
 
-recipeCloseBtn.addEventListener('click', () =>{
-    recipeDetailsContent.parentElement.style.display = "none";
-});
-searchBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const searchInput = searchBox.value.trim();
-    if(!searchInput)
-    {
-        recipeContainer.innerHTML = `<h2>Type the meal in the search box.</h2>`;
+
+async function checkWeather(city){
+    const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
         return;
     }
-    fetchRecipes(searchInput);
+
+    console.log("run");
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "/assets/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/assets/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/assets/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/assets/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/assets/snow.png";
+            break;
+
+    }
+
+    console.log(weather_data);
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
 });
