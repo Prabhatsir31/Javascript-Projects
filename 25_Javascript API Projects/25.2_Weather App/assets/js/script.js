89c4cea460f894db27c84e1dbda700aa165d3968
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-10-08 11:38:15 +0530

    Update script.js

--- 25_Javascript API Projects/25.2_Weather App/assets/js/script.js
+++ 25_Javascript API Projects/25.2_Weather App/assets/js/script.js
@@ -10,7 +10,6 @@ const location_not_found = document.querySelector('.location-not-found');
 
 const weather_body = document.querySelector('.weather-body');
 
-
 async function checkWeather(city){
     const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
@@ -34,7 +33,6 @@ async function checkWeather(city){
     humidity.innerHTML = `${weather_data.main.humidity}%`;
     wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
 
-
     switch(weather_data.weather[0].main){
         case 'Clouds':
             weather_img.src = "/assets/cloud.png";
@@ -56,7 +54,6 @@ async function checkWeather(city){
 
     console.log(weather_data);
 }
-
 
 searchBtn.addEventListener('click', ()=>{
     checkWeather(inputBox.value);
